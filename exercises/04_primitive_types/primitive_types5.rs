6355b2b585c8bea5c02fba4803b3a392124a926e
PlayfulMaven <cade4420@gmail.com>
2026-04-15 10:24:43 -0400

    finished 04_primitive_types

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -1,8 +1,9 @@
 fn main() {
-    let cat = ("Furry McFurson", 3.5);
+    let cat: (&str, f64) = ("Furry McFurson", 3.5);
 
-    // TODO: Destructure the `cat` tuple in one statement so that the println works.
-    // let /* your pattern here */ = cat;
+    // DONE: Destructure the `cat` tuple in one statement so that the println works.
+    let name = cat.0;
+    let age = cat.1;
 
-    println!("{name} is {age} years old");
+    println!("{} is {} years old", name, age);
 }
